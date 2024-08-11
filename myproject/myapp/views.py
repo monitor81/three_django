2bc4acedde28468105a7ac2a3a5b945bec177ac7
monitor81 <monitor81@mail.ru>
2024-08-11 19:33:39 +0500

    with database

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,7 @@
+# user - monit
+# pass - 1
+
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
@@ -6,25 +10,26 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        if username!='' and password != '':
-            response = HttpResponse("Login successful")
-            response.set_cookie('username', username)
-            return response
-        else:
-            return HttpResponse("Invalid login")
-    return render(request, 'myapp/login.html')
-    #     user = authenticate(request, username=username, password=password)
-    #     if user is not None:
-    #         login(request, user)
+    #     if username!='' and password != '':
     #         response = HttpResponse("Login successful")
     #         response.set_cookie('username', username)
     #         return response
     #     else:
     #         return HttpResponse("Invalid login")
     # return render(request, 'myapp/login.html')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponse("Login successful")
+            response.set_cookie('username', username)
+            return response
+        else:
+            return HttpResponse("Invalid login")
+    return render(request, 'myapp/login.html')
 
 def logout_view(request):
     logout(request)
-    response = HttpResponse("Logged out")
-    response.delete_cookie('username')
-    return response
+    # response = HttpResponse("Logged out")
+    # response.delete_cookie('username')
+    return HttpResponse("Logged out")
+    
